a0acb8aabee61e496b1346bf3e8d89a60cb5ff3b
Mira Fedas <30750556+myrosvas@users.noreply.github.com>
2022-04-17 11:38:43 +0200

    added CI

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.use(cors());
 app.use(json());
 app.use(router.routes()).use(router.allowedMethods())
 
+router.get('/', async (ctx) => {
+  return ctx.body = "Server is up an running on port 3001"
+})
+
 router.get('/blog', async (ctx) => {
   return axios.get('https://jsonplaceholder.typicode.com/posts')
   .then(({ data }) => {
